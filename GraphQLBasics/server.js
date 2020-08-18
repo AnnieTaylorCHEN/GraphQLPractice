19b63a0f74752caa4e0403766bb29c9e07a4f6db
Annie Taylor CHEN <annietaylorchen@gmail.com>
2020-08-19 00:04:26 +0200

    write graphql basic server.

--- GraphQLBasics/server.js
+++ GraphQLBasics/server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const { graphqlHTTP} = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
 app.use(
 	'/graphql',
 	graphqlHTTP({
+        schema,
 		graphiql: true,
 	})
 );
 
 app.listen(4000, () => {
-	console.log('server is starting at port 4000! ');
+	console.log('GraphQL server is running at port 4000! ');
 });
